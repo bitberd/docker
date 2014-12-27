358a82c83ea96848519ed747df0219ee206b0799
Alin Jurj <office@perfectweb.ro>
2014-12-28 00:01:01 +0200

    added docker.sh to startup

---
+++ docker.sh
@@ -0,0 +1,2 @@
+docker run -dit -p 80:80 -p 9000:9000 -v /var/run/mysqld/mysqld.sock:/var/run/mysqld/mysqld.sock -v $HOME/.ssh:/root/.ssh -v $HOME/Web/html:/var/www/html --name=development perfectweb/development
+docker run -dit -p 443:443 -p 9000:9000 -v /var/run/mysqld/mysqld.sock:/var/run/mysqld/mysqld.sock -v $HOME/Web/html/checkoutportal:/app --name=chportal chportal /bin/bash -c 'service apache2 restart & /bin/bash'
